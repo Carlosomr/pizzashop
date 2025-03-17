b3d7021abfe66d5cd82147b4b9a54a3979d288b8
Carlos Eduardo <carloseduardo.moura.contato@gmail.com>
2025-03-16 22:55:49 -0300

    pagina login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import {router} from './pages/router'
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster} from 'sonner'
 
 export function App() {
- return  <RouterProvider router={router} />
-  
+ return ( 
+ <HelmetProvider>
+    <Helmet titleTemplate="%s | pizza.shop"/>
+    <Toaster/>
+    <RouterProvider router={router} />
+ </HelmetProvider>
+ )
 }
--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,3 +1,11 @@
+import { Helmet } from "react-helmet-async";
+
 export function Dashboard(){
-    return <h1>dashboard</h1>
+    return (
+    <>
+    <Helmet title="Dashboard"/>
+    <h1>dashboard</h1>
+    </>
+ 
+    )
 }
