5583831a0b432806bedf642a0b220d941f4d6f0c
Carlos Eduardo <carloseduardo.moura.contato@gmail.com>
2025-04-15 23:35:17 -0300

    criando cards

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,34 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DollarSign } from "lucide-react";
+import { Helmet } from "react-helmet-async";
+
+export function Dashboard(){
+    return (
+    <>
+    <Helmet title="Dashboard"/>
+    <div className="flex flex-col gap-4">
+        <h1 className="text-3x1 font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+            <Card>
+                <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-base font-semibold">
+                        Receita Total (mês)
+                    </CardTitle>
+                    <DollarSign className="h-4 w-4 text-muted-foreground"/>
+                </CardHeader>
+                <CardContent className="space-y-1">
+                    <span className="text-2xl font-bold tracking-tight">
+                        R$ 1248,78
+                    </span>
+                    <p className="text-xs text-muted-foreground">
+                        <span className="text-emerald-500 dark:text-emerald-400">+2 %</span>
+                    </p>
+                </CardContent>
+            </Card>
+        </div>
+    </div>
+    </>
+ 
+    )
+}
